1576e9225d0dbaaffded233454120cbbe62ae09b
ra0648 <104158250+ra0648@users.noreply.github.com>
2022-11-11 12:19:56 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I'm Rupanjan , Currently a 2nd year undergraduate at SRM Chennai, enrolled for a btech in Computer Science Engineering As someone who enjoys learning new skills and currently developing interest in competitive coding,<br><br><br>I'm always looking for new ways in improving my skills. Additionally, I enjoy listening to music a great deal"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
